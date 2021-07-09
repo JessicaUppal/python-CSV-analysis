ee17c3039a319db94cc25b822d4f22e084faf707
Jessica <jessuppal22@gmail.com>
2021-07-09 17:32:45 +0100

    PyBank analysis homework

--- PyBank/main.py
+++ PyBank/main.py
@@ -44,6 +44,11 @@ with open(csvpath, newline='') as csvfile:
         monthly_change.append(revenue_change)
         previous_row = int(row[1])
         month_count.append(row[0])
+
+         # Calculate the average change 
+        average_change = sum(monthly_change)/ len(monthly_change)
+        highest = max(monthly_change)
+        lowest = min(monthly_change)
         
         # Calculate greatest increase in profits
         if int(row[1]) > greatest_increase:
@@ -55,13 +60,7 @@ with open(csvpath, newline='') as csvfile:
             greatest_decrease = int(row[1])
             greatest_decrease_month = row[0]  
         
-    # Calculate the average change 
-    average_change = sum(monthly_change)/ len(monthly_change)
-    
-    highest = max(monthly_change)
-    lowest = min(monthly_change)
-
-# Print Analysis
+# Print analysis
 print(f"Financial Analysis")
 print(f"---------------------------")
 print(f"Total Months: {total_months}")
@@ -69,3 +68,19 @@ print(f"Total: ${net_amount}")
 print(f"Average Change: ${average_change:.2f}")
 print(f"Greatest Increase in Profits:, {greatest_increase_month}, (${highest})")
 print(f"Greatest Decrease in Profits:, {greatest_decrease_month}, (${lowest})")
+
+# Specify file to write to
+analysis_output_file = os.path.join('.', 'analysis', 'budget_data_analysis.text')
+
+# Open the file using "write" mode. Specify the variable to hold the contents
+with open(analysis_output_file, 'w',) as txtfile:
+
+# Write the first row (column headers)
+    txtfile.write(f"Financial Analysis\n")
+    txtfile.write(f"---------------------------\n")
+# Write all remaining rows 
+    txtfile.write(f"Total Months: {total_months}\n")
+    txtfile.write(f"Total: ${net_amount}\n")
+    txtfile.write(f"Average Change: ${average_change}\n")
+    txtfile.write(f"Greatest Increase in Profits:, {greatest_increase_month}, (${highest})\n")
+    txtfile.write(f"Greatest Decrease in Profits:, {greatest_decrease_month}, (${lowest})\n")
