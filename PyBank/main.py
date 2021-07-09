b27e0d1743b6e03182abbc9e7a05a4df1f7af7a2
Jessica <jessuppal22@gmail.com>
2021-07-09 16:37:33 +0100

    PyBank homework

--- PyBank/main.py
+++ PyBank/main.py
@@ -31,3 +31,18 @@ with open(csvpath, newline='') as csvfile:
     greatest_increase = int(row[1])
     greatest_increase_month = row[0]
     
+    # Loop through the data
+    for row in csvreader:
+        
+        # Calculate Total Number Of Months 
+        total_months += 1
+        # Calculate Net Amount Of "Profit/Losses" Over The Entire Period
+        net_amount += int(row[1])
+
+        # Calculate change between the months
+        revenue_change = int(row[1]) - previous_row
+        monthly_change.append(revenue_change)
+        previous_row = int(row[1])
+        month_count.append(row[0])
+
+    
