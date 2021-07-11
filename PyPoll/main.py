34335a72195a453e9909f315ed1e4d24f4942cd0
Jessica <jessuppal22@gmail.com>
2021-07-11 08:57:42 +0100

    PyPoll analysis homework

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -19,4 +19,46 @@ with open(csvpath, newline='') as csvfile:
     
     # Read header row 
     csv_header = next(csvfile)
+for row in csvreader:
+        
+        # Calculate total votes cast
+        total_votes += 1
+        
+        # Calculate candidates that recieved votes
+        if (row[2] == "Khan"):
+            khan_votes += 1
+        elif (row[2] == "Correy"):
+            correy_votes += 1
+        elif (row[2] == "Li"):
+            li_votes += 1
+        else:
+            otooley_votes += 1
+            
+    # Percentage of votes each candidate won
+    khan_percent = khan_votes / total_votes
+    correy_percent = correy_votes / total_votes
+    li_percent = li_votes / total_votes
+    otooley_percent = otooley_votes / total_votes
+    
+    # Winner of the election based on popular vote
+    winner = max(khan_votes, correy_votes, li_votes, otooley_votes)
+
+    if winner == khan_votes:
+        winner_name = "Khan"
+    elif winner == correy_votes:
+        winner_name = "Correy"
+    elif winner == li_votes:
+        winner_name = "Li"
+    else:
+        winner_name = "O'Tooley" 
+
+# Print analysis
+print(f"Election Results")
+print(f"Total Votes: {total_votes}")
+print(f"Kahn: {kahn_percent:.3%}({khan_votes})")
+print(f"Correy: {correy_percent:.3%}({correy_votes})")
+print(f"Li: {li_percent:.3%}({li_votes})")
+print(f"O'Tooley: {otooley_percent:.3%}({otooley_votes})")
+print(f"Winner: {winner_name}")
+
 
